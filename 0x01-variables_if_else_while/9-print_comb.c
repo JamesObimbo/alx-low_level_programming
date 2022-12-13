f13f2a4df585fd3be5947e885722c794acc4e91f
JamesObimbo <jamesobimbo@gmail.com>
2022-12-13 15:00:50 -0800

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Numbers must be separated by ,, followed by a space
+ * Numbers should be printed in ascending order
+ * use putchar only for printing
+ * can only use putchar four times maximum in your code
+ * cannot use char variable
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
