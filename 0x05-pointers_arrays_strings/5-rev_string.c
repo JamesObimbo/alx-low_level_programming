ebb6119d20bdb17eb37576eee49c7ac1c8949f74
JamesObimbo <jamesobimbo@gmail.com>
2022-12-20 08:33:26 -0800

    a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,56 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rev_string - Reverses a string
  * @s: The string to be modified
+ * 
  * Return: void
  */
+
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
+	int len, i;
+	char *begin, *end, temp;
 
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
+	len = string_length(s); /* Get function from the string_length.c */
+	begin = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+		end++;
+
+	for for (i = 0; i < len / 2; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		/**
+		 * The idea is to swap the beginning and end pointers
+		 * of the string. Instead of swapping the elements, here
+		 * we will swap the pointers where the addreses of the
+		 * input string are stored.
+		 */
+
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
 	}
+}
+
+/**
+ * string_length - Function that returns string length
+ * @s: Pointer to the string return length
+ *
+ * Return: The length of the string.
+ */
+
+int string_length(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '/0')
+		length++;
+
+	return (length);
 }
