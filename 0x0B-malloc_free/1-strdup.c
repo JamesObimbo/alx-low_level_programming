8f615f1e307dcf8a3e58b614642221d2f817639c
JamesObimbo <jamesobimbo@gmail.com>
2023-01-10 11:09:23 -0800

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,34 +4,30 @@
 
 /**
  * _strdup - function that returns a pointer to a newly allocated space,
- * in memory, which contains a copy of the string given as a parameter.
- * @str: string to be duplicated.
- * Return: NULL (fail) or a pointer to the duplicated string (success).
- * Also return NULL if str = NULL or insufficient memory is available.
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: string to be duplicated
+ * Return: NULL (fail) or a pointer to the duplicated string (success)
+ * Also return NULL if str = NULL or insufficient memory is available
  */
 
 char *_strdup(char *str)
 {
-	char *cop;
-	unsigned int i;
+	char *dupStr;
+	unsigned int i, j;
 
-	/* if str is NULL, return NULL */
 	if (str == NULL)
 		return (NULL);
 
-	/* allocate enough memory for cop */
-	cop = malloc(_strlen(str) + 1);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-	/* if malloc fails, return NULL */
-	if (cop == NULL)
+	dupStr = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (dupStr == NULL)
 		return (NULL);
 
-	/* copy string inside str to cop */
-	for (i = 0; str[i] != '\0'; i++)
-		cop[i] = str[i];
+	for (j = 0; j <= i; j++)
+		dupStr[j] = str[j];
 
-	/* append null terminator to cop */
-	cop[i] = '\0';
-
-	return (cop);
+	return (dupStr);
 }
