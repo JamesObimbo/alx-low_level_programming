4b4016ff83f8e1879121eb072a7eba7752bf66da
JamesObimbo <jamesobimbo@gmail.com>
2023-01-13 11:28:51 -0800

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,7 +20,6 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
-
 	return (1);
 }
 
@@ -38,7 +37,6 @@ int _strlen(char *s)
 	{
 		i++;
 	}
-
 	return (i);
 }
 
@@ -71,13 +69,10 @@ int main(int argc, char *argv[])
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-	
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -89,20 +84,16 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
-
 		if (a)
 			_putchar(result[i] + '0');
 	}
-
 	if (!a)
 		_putchar('0');
 	
